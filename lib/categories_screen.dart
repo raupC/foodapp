ed9e70d79e8a54b95f38eb883f53d1011a032d7e
Lordracfures <raul.pineda.caballero@gmail.com>
2023-02-16 20:52:46 +0200

    firsts version

---
+++ lib/categories_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'category_item.dart';
+import 'dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("DeliMeals"),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(15),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 2 / 3,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          children: DUMMY_CATEGORIES
+              .map((catData) =>
+                  CategoryItem(title: catData.title, color: catData.color))
+              .toList()),
+    );
+  }
+}
---
+++ lib/dummy_data.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import './models/Category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(id: 'c1', title: 'Italian', color: Colors.purple),
+  Category(id: 'c2', title: 'Quieck &  Easy', color: Colors.green),
+  Category(id: 'c10', title: 'Summer', color: Colors.blue),
+  Category(id: 'c10', title: 'Summer', color: Colors.pink),
+  Category(id: 'c10', title: 'Summer', color: Colors.lime),
+  Category(id: 'c10', title: 'Summer', color: Colors.orange),
+  Category(id: 'c10', title: 'Summer', color: Colors.indigo),
+  Category(id: 'c10', title: 'Summer', color: Colors.teal),
+];
